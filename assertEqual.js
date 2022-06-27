07db6eb9093342080107ed551ccc37f758d9963c
ArenRitz <aren.dolbakian@gmail.com>
2022-06-27 15:48:45 -0400

    Added function to check equality of 2 args

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 🟢 🟢 Assertion Passed: ${actual} === ${expected} 🟢 🟢 🟢 `);
+  } else {
+    console.log(`🔴 🔴 🔴 Assertion Failed: ${actual} !== ${expected} 🔴 🔴 🔴 `);
+  }
+};
+
