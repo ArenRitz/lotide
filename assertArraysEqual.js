3a94041fbbe4ded789d46aed35ca2b4033dae62a
ArenRitz <aren.dolbakian@gmail.com>
2022-06-28 18:25:49 -0400

    added assertArraysEquals.js which checks if two arrays are euqual and prints confirmation to console

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+const assertArraysEqual = function(arr1, arr2) {
+  const sucessMsg = `🟢 🟢 🟢 Arguments match: ${arr1} === ${arr2} 🟢 🟢 🟢 `;
+  const failMsg = `🔴 🔴 🔴 Arguments DO NOT match: ${arr1} === ${arr2} 🔴 🔴 🔴 `;
+    let isEqual = false;
+    if (arr1.length === arr2.length) {
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] === arr2[i]) {
+          isEqual = true;
+        } else if (arr1[i] !== arr2[i]) {
+          console.log(failMsg);
+          return;
+        }
+      }
+    }
+    if (isEqual) {
+      console.log(sucessMsg);
+    } else {
+      console.log(failMsg);
+    }
+  };
+  
+  
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 0], [1, 2, 3]);
+assertArraysEqual([1, 0, 3], [1, 2, 3]);
+assertArraysEqual([0, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3])
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
