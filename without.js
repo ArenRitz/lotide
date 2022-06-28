d8f42f0cdab2c29dcfd15de257fe3f33da7bf5ab
ArenRitz <aren.dolbakian@gmail.com>
2022-06-28 19:11:08 -0400

    Added function with 2 array parameters which removes from the first  all the items that match the second array

---
+++ without.js
@@ -0,0 +1,40 @@
+const assertArraysEqual = function (arr1, arr2) {
+  const sucessMsg = `🟢 🟢 🟢 Arguments match: ${arr1} === ${arr2} 🟢 🟢 🟢 `;
+  const failMsg = `🔴 🔴 🔴 Arguments DO NOT match: ${arr1} === ${arr2} 🔴 🔴 🔴 `;
+  let isEqual = false;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        isEqual = true;
+      } else if (arr1[i] !== arr2[i]) {
+        console.log(failMsg);
+        return;
+      }
+    }
+  }
+  if (isEqual) {
+    console.log(sucessMsg);
+  } else {
+    console.log(failMsg);
+  }
+};
+
+const without = function (source, itemsToRemove) {
+  let newArr = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArr.push(source[i])
+    }
+  }
+  //console.log(source) --chekc if original array is modified
+  return newArr
+}
+
+/*
+const array1 = ["apple", "candy", "chocolate", "bugs", "dog"];
+const array2 = ["apple", "bugs"];
+
+
+assertArraysEqual(without(array1, array2), ["candy", "chocolate", "dog"]);
+assertArraysEqual(without(array1, array2), ["candy", "chocolate", "bat"]);
+*/
