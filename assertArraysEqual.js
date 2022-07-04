bf89589054d7b0a1b5ecde3dca61e21efe13e9fa
ArenRitz <aren.dolbakian@gmail.com>
2022-07-04 16:49:59 -0400

    Refactored assertArraysEqual function and moved tests to /test directory

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  let isEqual = false;
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        isEqual = true;
-      } else if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  }
-  return isEqual;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   const sucessMsg = `🟢 🟢 🟢 Arguments match: ${arr1} === ${arr2} 🟢 🟢 🟢 `;
@@ -22,14 +10,14 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
+module.exports = assertArraysEqual;
 
 
 
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 0], [1, 2, 3]);
-assertArraysEqual([1, 0, 3], [1, 2, 3]);
-assertArraysEqual([0, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3, 4], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 0], [1, 2, 3]);
+// assertArraysEqual([1, 0, 3], [1, 2, 3]);
+// assertArraysEqual([0, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3, 4], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
